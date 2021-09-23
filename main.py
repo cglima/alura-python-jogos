75f07f36fa7f2428ab69c90ffd3d155db59ea100
Cassiana <lima.cglima@gmail.com>
2021-09-22 23:29:01 -0300

    inicio do jogo de adivinhação, abordando entradas do usuário, testando valores, a sequencia do jogo e iterando de formas diferentes.

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("Hello World")
+print(20 * "=")
+print("Jogo de adivinhação")
+print(20 * "=")
+
+numero_secreto = 5
+total_de_tentativas = 3
+rodada = 1
+
+while(rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+
+    chute = int(input("Digite um número: "))
+    print("você digitou o número", chute)
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if(acertou):
+        print("Parabéns! Você acertou o número secreto")
+    else:
+        if (maior):
+            print("Você errou! O seu chute é maior que o número secreto")
+        elif(menor):
+            print("Você errou! O número digitado é menor que o número secreto")
+
+    rodada = rodada +1
+
+print(20 * "=")
+print("Fim de jogo")
+print(20 * "=")
