386deca769dc485a3f8cb133821f2178511f77e5
Cassiana <lima.cglima@gmail.com>
2021-09-26 13:02:35 -0300

    inicializando a construção do jogo da forca. foi inserido no programa as variaveis e laço condicional while para que o jogo continue até que o usuário acerte ou perca as chances de acertar a palavra secreta.

--- forca.py
+++ forca.py
@@ -3,6 +3,13 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print(20 * "*")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("Jogando...")
+
     print(20 * "=")
     print("Fim de jogo")
     print(20 * "=")
