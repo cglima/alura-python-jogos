a0888c6f0cd3f93fa6dbb8ddb76e8446915287f2
Cassiana <lima.cglima@gmail.com>
2021-09-26 13:30:21 -0300

    manipulando strings, buscando um caracter em uma string

--- forca.py
+++ forca.py
@@ -7,7 +7,17 @@ def jogar():
     enforcou = False
     acertou = False
 
-    while(not enforcou and not acertou):
+    while(not acertou and not enforcou):
+
+        chute = input("Qual a letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+                index = index + 1
+
         print("Jogando...")
 
     print(20 * "=")
